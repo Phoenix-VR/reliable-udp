f4fc439848f69a0637cfb4b9e16fea98a78da0b0
Phoenix-VR <86906691+Phoenix-VR@users.noreply.github.com>
2023-07-04 13:56:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--from_port",type=int)
-parser.add_argument("--to_port",type=int)
+parser.add_argument("--send",type=str)
 args = parser.parse_args()
 
 sock = main.Transmission(src_port=args.from_port)
+ip,port = str(args.send).split(":")
+sock.connect(target_addr=(ip,int(port)),timeout=10)
 
-sock.connect(target_addr=("192.168.0.105",args.to_port),timeout=10)
